7c5adc366899c83b5b9b3174cf43fc99733d45e2
Jairo Güemes <jairoguemes05@iesamachado.org>
2023-12-15 14:28:34 +0100

    javascript

---
+++ JavaScript/fichero.js
@@ -0,0 +1,7 @@
+/*Conjunto de funciones realizadas por Jairo Güemes 
+para el sitio web https://www.dominio.com */
+// esto es un comentario de una línea
+
+function saludo() {
+    alert('Hola hackers!!!!!!!!')
+}
