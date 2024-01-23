e6f468f07d3a498555b607d3f3a5f53d576d1cb7
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-01-23 08:53:23 +0100

    arrays intentadas

--- JavaScript/ejarrays.js
+++ JavaScript/ejarrays.js
@@ -1,3 +1,23 @@
-function añadir ()  {
-    
+const palabras=["Comer","Beber","Dormir","Jugar"]
+function añadir (array)  {
+    document.getElementById("anadir").innerHTML = palabras;
+    palabras.push("Experimentar");
 }
+
+function eliminar (array)   {
+    document.getElementById("anadir").innerHTML = palabras;
+    palabras.pop();
+}
+function creciente (array){
+    document.getElementById("anadir").innerHTML = palabras;
+    palabras.sort();
+}
+function decreciente (array){
+    document.getElementById("anadir").innerHTML = palabras;
+    palabras.reverse();
+}
+function posicion (array){
+    document.getElementById("position").innerHTML = palabras;
+    palabras.indexOf("Comer");
+}
+
