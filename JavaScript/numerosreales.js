6cd1fafb1579224360c1fe90541681a9c9ca55a6
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-01-25 22:10:40 +0100

    dddd

--- JavaScript/numerosreales.js
+++ JavaScript/numerosreales.js
@@ -1,14 +1,7 @@
-var numeros = parseFloat(document.getElementById ("numeros").value);
-function anadir ()  {
-    document.getElementById("nreales").innerHTML = numeros;
-    numeros.push();
-}
+var numeros = ["1","2","3"]
+document.getElementById("nreales").innerHTML = numeros;
 
-function sumar ()   {
-    document.getElementById("nreales").innerHTML = numeros;
-    parseFloat() * numeros;
-}
-
-function multiplicar()  {
-    document.getElementById("nreales").innerHTML = numeros;
+function anadir () {
+document.getElementById("nreales").innerHTML = numeros;
+numeros.push();
 }
