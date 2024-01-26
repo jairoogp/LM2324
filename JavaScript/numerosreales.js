a637f2787fdb01b94aa0700c422d44c9c6ec3e03
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-01-26 14:29:13 +0100

    sasde

--- JavaScript/numerosreales.js
+++ JavaScript/numerosreales.js
@@ -1,7 +1,24 @@
-var numeros = ["1","2","3"]
-document.getElementById("nreales").innerHTML = numeros;
+var numeros = []
 
 function anadir () {
-document.getElementById("nreales").innerHTML = numeros;
-numeros.push();
+    var n =parseFloat(document.getElementById("n").value);
+    numeros.push(n);
+    document.getElementById("nreales").innerHTML = numeros
+}
+
+var s = 0  // solo se ejecuta la primera vez 
+function sumar () {
+    s = 0;          // se ejecuta cada vez que le demos a sumar
+    for (var i = 0; i<numeros.length; i++)  {
+        s += numeros[i];
+    }
+    document.getElementById("nreales").innerHTML = s
+}
+
+function multiplicar()  {
+    var m = 1
+    for (var i = 0; i< numeros.length; i++) {       //for se repite , inicializacion,condicion,incremento
+        m = m * numeros[i];
+    }
+    document.getElementById("nreales").innerHTML = m;
 }
