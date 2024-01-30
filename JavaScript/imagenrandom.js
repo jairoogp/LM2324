a3e149ac27feb23359d5bb913f2a1c06052eff68
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-01-30 18:28:06 +0100

    sumapareshechos imagen queda

---
+++ JavaScript/imagenrandom.js
@@ -0,0 +1,5 @@
+var escudos = ["images/imagen1.jpg","iamges/imagen2.jpg"]
+
+function mostrarImagen() {
+    document.getElementById("resultado").innerHTML = escudos;
+}
---
+++ JavaScript/sumapares.js
@@ -0,0 +1,15 @@
+var numeros = [4,8,9,11,18,22,21,23,26]
+
+function ver () {
+    document.getElementById("mostrar").innerHTML = numeros;
+}
+
+var r = 0
+function  devolver ()   {
+    for (var i=0; i<numeros.length; i++) {
+        r += numeros[i]
+    }
+    document.getElementById("resultado").innerHTML = "Sumando los numeros pares el resultado queda:" + r;
+
+
+}
