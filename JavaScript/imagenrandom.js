9b7843745c86cef91d41d84fb0d4370607845f3d
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-01-30 18:36:38 +0100

    sphechos

--- JavaScript/imagenrandom.js
+++ JavaScript/imagenrandom.js
@@ -1,5 +1,14 @@
-var escudos = ["images/imagen1.jpg","iamges/imagen2.jpg"]
+function numero(x)  {
+    return(Math.floor(Math.random()* x + 1));
+}
+
+var escudos = newArray()
+escudo[0] = "imagen1.jpg";
+escudo[1] = "imagen2.jpg";
+escudo[2] = "imagen4.png";
+escudo[3] = "imagen5.jpg";
+escudo[4] = "imagen6.png";
 
 function mostrarImagen() {
-    document.getElementById("resultado").innerHTML = escudos;
+    document.getElementById("resultado").src = escudos[numero(4)-1];
 }
