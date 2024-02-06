29b6973ca0081d8224eed4bd402aa4a738cf96dc
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-02-06 20:23:44 +0100

    intentadas calificacionez

--- JavaScript/calificaciones.js
+++ JavaScript/calificaciones.js
@@ -3,9 +3,9 @@ var calificaciones = []
 function calificacion  ()   {
     var n = parseFloat(document.getElementById("c").value);
     calificaciones.push(n);
-    document.getElementById("calificaciones").innerHTML = calificaciones;
+    document.getElementById("resultado").innerHTML = calificaciones;
 
-    var numeroCorrecto = parseFloat(numero)
+    var numeroCorrecto = parseFloat(n)
     if (numeroCorrecto >=0 && numeroCorrecto<=10) {
        return numeroCorrecto;
     }
@@ -14,4 +14,19 @@ function calificacion  ()   {
         return null;
     }
 
+}
+function borrar ()  {
+    var cadena = document.getElementById("c").value;
+    document.getElementById("resultado").innerHTML = ("");
+    
+}
+
+var r = 0 
+function mediaCalificacion ()   {
+    r = 0;
+    for (var i=0; i<calificaciones.length;i++)  {
+        r += calificaciones[i] ; // es lo mismo que poner r = r + calificaciones[i]
+    }
+    var media = r / calificaciones.length;
+    document.getElementById("resultado").innerHTML = ("El resultado de la media es ") + media ;
 }
