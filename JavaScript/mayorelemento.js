38313a91b4f77ee7d82bfc3fda5632d161300937
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-02-09 14:29:07 +0100

    hecho viernes

--- JavaScript/mayorelemento.js
+++ JavaScript/mayorelemento.js
@@ -1,12 +1,27 @@
-var numeros = [1,3,9,0,45,98,12,24,64,32,35,567,34]
+
 
 function mayor (vector)    {
     let mayor = vector[0]; // por defecto el primer elemento es el mayor
+
     for(var i=0;i<vector.length;i++){
         if(vector[i]>mayor)
         mayor = vector[i];
-
     }
     return mayor;
     
+}
+
+function menor (vector) {
+    let menor = vector[0];
+    for(var i=0;i<vector.length;i++){
+        if(vector[i]<menor)
+        menor = vector[i];
+    }
+    return menor;
+}
+
+function imprimir (vector)  {
+    for(var contador=0;contador<vector.length;contador++)   {
+        document.getElementById("resultado").innerHTML = document.getElementById("resultado").innerHTML + vector[contador] + "<br/>" 
+    }
 }
