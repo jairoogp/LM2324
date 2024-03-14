e81e30aca1ce388f786ba877ffb4d756b36296f1
Jairo Güemes <jairoguemes05@iesamachado.org>
2024-03-14 18:03:35 +0100

    cambios almacenados

--- JavaScript/localstorage(II)/carrito.js
+++ JavaScript/localstorage(II)/carrito.js
@@ -7,8 +7,8 @@ let carro = [
 let almacenado=null;
 function iniciar(){
     almacenado = localStorage.getItem("carro");
-    if (****************){ // Comprobamos si el carrito tenía algo almacenado 
-        almacenado = ****************; // Convertimos el texto almacenado en un objeto Javascript
+    if (almacenado!=null){
+        almacenado = JSON.parse(almacenado);
         carro = almacenado;
         refrescar();
     }
@@ -19,14 +19,14 @@ function anadir(producto){
      objeto['uds'] += 1; // Añado unidad
      carro[producto] = objeto;
      document.getElementById("id"+producto).value = objeto['uds'];
-     **************** // Almacenamos en localStorage el carro
+     localStorage.setItem("carro", JSON.stringify(carro));
      refrescar();
 }
 function quitar(producto){
     // En producto llevamos el identificador del producto a guardar en el carrito
     let objeto = carro[producto];
     let numuds = objeto['uds'];
-    if (****************){ // Comprobamos que el número de unidades sea al menos una
+    if (numuds>=1){
         objeto['uds'] -= 1; // Quito unidad
         carro[producto] = objeto;
         document.getElementById("id"+producto).value = objeto['uds'];
@@ -35,22 +35,22 @@ function quitar(producto){
     }
 }
 // Actualiza visualización carrito y almacenamiento local
-****************{
+function refrescar(){
     let listaCarrito = document.getElementById("carrito");
     let total=0;
     listaCarrito.innerHTML="";
-    **************** // Recorremos todos los elementos del carrito
+    for (var i=0;i<carro.length;i++)
     {
         
         let producto = carro[i];
         if (producto['uds']>0){
-            let nodo = ****************; // Creamos un nuevo elemento lista
+            let nodo = document.createElement("li");
             let texto = document.createTextNode("Producto: " + producto['titulo']+" Unidades:"+producto['uds']+" Precio ud:"+producto['precio']);
             document.getElementById("id"+i).value = producto['uds'];
-            **************** // Añadimos el texto
+            nodo.appendChild(texto);
             listaCarrito.appendChild(nodo);
             total = total + producto['uds']*producto['precio'];
         }
     }
-    **************** // Escribimos el total
+    document.getElementById("datos").innerHTML = "Total: " + total.toFixed(2) + "€";
 }
